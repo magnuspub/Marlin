f50c4b6dd94d2989c417b6aced8e9a8616b1ecee
Federico <magnus@gmx.it>
2017-06-10 19:06:25 +0200

    modifica config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -494,7 +494,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 10, 10 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 10, 100 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
